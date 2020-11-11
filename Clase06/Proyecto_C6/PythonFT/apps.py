41c8e59328d3a60fc8d0c37bee8d30e478529a29
CaidevOficial <falcone.facu@gmail.com>
2020-11-11 16:17:09 -0300

    Delete Clase06

--- Clase06/Proyecto_C6/PythonFT/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PythonftConfig(AppConfig):
-    name = 'PythonFT'
--- Clase06/Proyecto_C6/Tasks/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-tasks = ["Estudiar","Tomar Cafe", "Dormir"]
-
-def index(request):
-    return render(request, "Tasks/index.html", {"Tasks":tasks})
