146c9e0ee6691dfcb1d44f9da8c2ef55490b950e
CaidevOficial <falcone.facu@gmail.com>
2020-11-26 14:48:45 -0300

    Update

--- Python_FTPoloTic_[C10]/Clase_10/Clase_10/settings.py
+++ Python_FTPoloTic_[C10]/Clase_10/Clase_10/settings.py
@@ -76,9 +76,15 @@ WSGI_APPLICATION = 'Clase_10.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.mysql',
+        'OPTIONS' : {
+            'read_default_file':'//etc//mysql//mariadb.cnf',
+        },
     }
 }
 
